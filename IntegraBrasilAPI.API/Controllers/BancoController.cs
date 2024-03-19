ebc1aabc7c958c8e489c86f63d68652730f803cd
Paulo de Tarso <potassio@hotmail.com>
2024-03-19 10:55:54 -0300

    Criação das controllers Banco e endereço

---
+++ IntegraBrasilAPI.API/Controllers/BancoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntegraBrasilAPI.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BancoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index() => Ok();
+    }
+}
---
+++ IntegraBrasilAPI.API/Controllers/EnderecoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntegraBrasilAPI.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnderecoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index() => Ok();
+    }
+}
